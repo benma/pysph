d55636b2deee09fe7e17909bf7b431bee12db3ea
Marko Bencun <mbencun@gmail.com>
2012-05-01 09:45:34 +0200

    Find Cg libs also on Windows. Fix possibly missing definition of M_PI.

--- src/cg/platform.py
+++ src/cg/platform.py
@@ -1,5 +1,12 @@
+import os
 import OpenGL.platform # cgGl depends on it
 
 from ctypes import *
-cg_platform = CDLL('libCg.so')
-cg_gl_platform = CDLL('libCgGL.so')
+
+if os.name == 'nt':
+    # we are on Windows
+    cg_platform = CDLL('Cg.dll')
+    cg_gl_platform = CDLL('CgGL.dll')
+else:
+    cg_platform = CDLL('libCg.so')
+    cg_gl_platform = CDLL('libCgGL.so')
--- src/sph/sph.cl
+++ src/sph/sph.cl
@@ -1,3 +1,6 @@
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 typedef struct Params {
   uint N;
